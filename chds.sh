905eb817b7b367f4cd7ee5845072442d534f09c5
Cassiano <cassianoperin@gmail.com>
2020-04-26 23:18:35 -0300

    Added CHD download script

---
+++ chds.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Need an exported report from Romcenter (Miss CHDs)
+
+# Variables
+URL="https://bda.retroroms.info:82/downloads/mame/CHDs"
+report_name='chds_miss.txt' # CHD Foldersinside URL Path
+username=''
+password=''
+
+
+# Retrieve the CHD list
+for CHD in $(cat $report_name) ; do
+
+	# List the rom files (.rar) inside CHD folder and download
+	for FILE_NAME in $(curl $URL/$CHD/ | grep -i rar | cut -d' ' -f3 | cut -d '"' -f2 ) ; do
+		wget -r -np -nc  --http-user=$username --http-password=$password -A rar $URL/$CHD/$FILE_NAME 
+	done
+
+done
